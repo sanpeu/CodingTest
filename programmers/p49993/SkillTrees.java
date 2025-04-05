5659b33d49c872a2f10a6df72decabeea3504a78
sanpeu <udbmmuk41@naver.com>
2025-04-05 12:31:21 +0900

    Skill Tree solve1

--- programmers/p49993/SkillTrees.java
+++ programmers/p49993/SkillTrees.java
@@ -1,28 +1,22 @@
 package programmers.p49993;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = -1;
-        Pattern pattern = Pattern.compile(toRegex(skill));
-        Matcher matcher;
-
+        int answer = 0;
         for (var skill_tree: skill_trees) {
-            matcher = pattern.matcher(skill_tree);
-            if (matcher.find()) ++answer;
+            ++answer;
+            for (int i = skill.length()-1; i > 0; --i)  {
+                int currentIndex = skill_tree.indexOf(skill.charAt(i));
+                int preIndex = skill_tree.indexOf(skill.charAt(i - 1));
+
+                if (currentIndex == -1) continue;
+                if (currentIndex < preIndex || preIndex == -1) {
+                    --answer;
+                    break;
+                }
+            }
         }
         return answer;
-    }
-
-    public String toRegex(String string) {
-        StringBuilder regex = new StringBuilder();
-        for (int i = 0; i < string.length(); ++i) {
-            regex.append(string.charAt(i));
-            regex.append("[A-Z]*");
-        }
-
-        return regex.toString();
     }
 }
 
