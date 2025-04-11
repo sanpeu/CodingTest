b10ba6cdd8f4846d3101dcf48202759d7028bff3
sanpeu <udbmmuk41@naver.com>
2025-04-11 23:18:00 +0900

    medianFinder v3

--- beakjoon/p1655/MedianFinder.java
+++ beakjoon/p1655/MedianFinder.java
@@ -3,29 +3,41 @@ package beakjoon.p1655;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MedianFinder {
+    public static void balanceQueue(PriorityQueue smallerHeap, PriorityQueue biggerHeap) {
+        // smaller만 너무 커지는 것 조절
+        while (smallerHeap.size() - biggerHeap.size() > 1)
+            biggerHeap.offer(smallerHeap.poll());
+
+        // 항상 smallerHeap이 bigger보다 1개 크거나 같아야함.
+        while (biggerHeap.size() - smallerHeap.size() >= 1)
+            smallerHeap.offer(biggerHeap.poll());
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(reader.readLine());
-        List<Integer> numbers = new ArrayList<>();
+        Integer[] numbers = new Integer[N];
+
+        PriorityQueue<Integer> biggerHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> smallerHeap = new PriorityQueue<>(Comparator.reverseOrder());
 
         for (int i = 0; i < N; ++i)
-            numbers.add(Integer.parseInt(reader.readLine()));
+            numbers[i] = Integer.parseInt(reader.readLine());
         reader.close();
 
-        Integer median = 0;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; ++i) {
-            PriorityQueue<Integer> heap = new PriorityQueue<>(numbers.subList(0, i+1));
-            double iterationCount = heap.size()/2.0;
+            if (!smallerHeap.isEmpty() && numbers[i] > smallerHeap.peek())
+                biggerHeap.offer(numbers[i]);
+            else
+                smallerHeap.offer(numbers[i]);
 
-            for (int j = 0; j < iterationCount; ++j)
-                median = heap.poll();
-            sb.append(median).append("\n");
+            balanceQueue(smallerHeap, biggerHeap);
+            sb.append(smallerHeap.peek()).append("\n");
         }
 
         System.out.println(sb);
